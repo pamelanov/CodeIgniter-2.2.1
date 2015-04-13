e94bd98e9858d5e8af7001866ca0a02f82a76fc3
pamelanov <novranska111@gmail.com>
2015-04-13 13:57:46 +0700

    nambahin isi welcome

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -17,10 +17,80 @@ class Welcome extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see http://codeigniter.com/user_guide/general/urls.html
 	 */
-	public function index()
-	{
-		$this->load->view('welcome_message');
+	public function index() {
+		$data['judul'] = "Halaman Depan";
+		$data['main'] = "home/home";
+		$data['aktif'] = 'class="active"';
+	
+		$this->load->view('home/template', $data);
 	}
+	
+	function __construct() {
+		parent::__construct();
+	
+		$this->load->model('', '', '', 'madmins', true);
+	}
+	
+	
+	public function contact() {
+		$data['judul'] = "Contac Us";
+		$data['main'] = "home/contact";
+		$data['aktif'] = 'class="active"';
+	
+		$this->load->view('home/template', $data);
+	}
+	
+	public function about() {
+		$data['judul'] = "About Us";
+		$data['main'] = "home/about";
+		$data['aktif'] = 'class="active"';
+		$this->load->view('home/template', $data);
+	}
+	
+	
+	public function login() {
+		$data['judul'] = "Halaman Login";
+		$data['main'] = "home/login";
+		$data['aktif'] = 'class="active"';
+	
+		$this->load->view('home/template', $data);
+	}
+	
+	function ceklogin() {
+		$this->load->library('encrypt');
+		if ($this->input->post('username')) {
+			$u = $this->input->post('username');
+			$pw = $this->input->post('password');
+			$row = $this->madmins->verifyUser($u, $pw);
+			if (count($row)) {
+				$_SESSION['userid'] = $row['id'];
+				$_SESSION['role'] = 1;
+	
+				redirect('admin/dashboard', 'refresh');
+			} else {
+				$this->session->set_flashdata('result', 'maap username atau password Anda salah!');
+				redirect('template/login', 'refresh');
+			}
+		} else {
+			$this->session->set_flashdata('result', 'Isi Username dan password dulu!');
+			redirect('template/login', 'refresh');
+		}
+	}
+	
+	function logout() {
+		$sesi_items = array('user' => '');
+		$this->session->unset_userdata($sesi_items);
+		$this->session->set_flashdata('result', 'Anda wes logout');
+		header('location:' . base_url() . 'index.php/template/login');
+	}
+	
+	
+	
+	
+	
+	}
+	
+	?>
 }
 
 /* End of file welcome.php */
