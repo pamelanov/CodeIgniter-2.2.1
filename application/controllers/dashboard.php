3e1633645460b629d8c62dd5c77341836725cadb
idoooy <yodi.syaeful@gmail.com>
2015-04-17 16:15:53 +0700

    akses role update

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -1,12 +1,11 @@
 <?php
 
 class Dashboard extends Ci_Controller {
-        
-        
+
     function __construct() {
         parent::__construct();
-        if($this->session->userdata('role') != 1 && $this->session->userdata('role') != 2 && $this->session->userdata('role') != 3) {
-           redirect('template/login', 'refresh');
+        if ($this->session->userdata('role') != 1 && $this->session->userdata('role') != 2 && $this->session->userdata('role') != 3) {
+            redirect('template/login', 'refresh');
         }
     }
 
@@ -16,46 +15,53 @@ class Dashboard extends Ci_Controller {
         $this->load->vars($data);
         $this->load->view('dashboard');
     }
-    
-          
 
-    function createData(){
-	$data['judul'] = "Create";
+    function createData() {
+        $data['judul'] = "Create";
         $data['main'] = 'create';
         $this->load->vars($data);
         $this->load->view('dashboard');
     }
-    
-    function createStatus(){
-	$data['judul'] = "Create Status";
+
+    function createStatus() {
+        $data['judul'] = "Create Status";
         $data['main'] = 'ops/createStatus';
         $this->load->vars($data);
         $this->load->view('dashboard');
     }
-    
-    function refunds(){
+
+    function refunds() {
+        if ($this->session->userdata('role') != 2 && $this->session->userdata('role') != 3) {
+            redirect('dashboard', 'refresh');
+        }
         $r = new Refund();
-        
+
         $data['judul'] = "Refund Summary";
         $data['main'] = 'ops/refund_home';
         $data['admins'] = $r->getAllRefunds();
         $this->load->vars($data);
         $this->load->view('dashboard');
     }
-    
-    function feedbacks(){
-    	$r = new Feedback();
-    
-    	$data['judul'] = "Feedback Summary";
-    	$data['main'] = 'ops/feedback_home';
-    	$data['feedback'] = $r->getAllFeedbacks();
-    	$this->load->vars($data);
-    	$this->load->view('dashboard');
+
+    function feedbacks() {
+        if ($this->session->userdata('role') != 2 && $this->session->userdata('role') != 3) {
+            redirect('dashboard', 'refresh');
+        } 
+        $r = new Feedback();
+
+        $data['judul'] = "Feedback Summary";
+        $data['main'] = 'ops/feedback_home';
+        $data['feedback'] = $r->getAllFeedbacks();
+        $this->load->vars($data);
+        $this->load->view('dashboard');
     }
-    
+
     function users() {
+        if ($this->session->userdata('role') != 1) {
+            redirect('dashboard', 'refresh');
+        }
         $o = new Account();
-        
+
         $data['judul'] = "Manage Users";
         $data['main'] = 'admin/user_home';
         $data['admins'] = $o->getAllAccounts();
