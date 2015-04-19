9cef6de1b796a387d918bdcc3ca77747c1cfbeca
idoooy <yodi.syaeful@gmail.com>
2015-04-20 00:33:43 +0700

    edited

--- application/views/admin/user_create.php
+++ application/views/admin/user_create.php
@@ -1,37 +1,39 @@
-<h1><?php echo $judul;?></h1>
-
-<?php
-echo form_open('admin/dashboard/create');
-echo "<p><label for='uname'>Nama</label><br/>";
-$data = array('name'=>'username','id'=>'uname','size'=>25);
-echo form_input($data) ."</p>";
-
-echo "<p><label for='email'>Email</label><br/>";
-$data = array('name'=>'email','id'=>'email','size'=>25);
-echo form_input($data) ."</p>";
-
-echo "<p><label for='uname'>Telp</label><br/>";
-$data = array('name'=>'telp','id'=>'uname','size'=>25);
-echo form_input($data) ."</p>";
-
-echo "<p><label for='uname'>Alamat</label><br/>";
-$data = array('name'=>'alamat','id'=>'uname','size'=>50);
-echo form_input($data) ."</p>";
-
-echo "<p><label for='pw'>Password</label><br/>";
-$data = array('name'=>'password','id'=>'pw','size'=>30);
-echo form_password($data) ."</p>";
-
-echo "<p><label for='uname'>Role</label><br/>";
-$data = array('name'=>'role','id'=>'uname','size'=>1    );
-echo form_input($data) ."</p>";
-
-echo "<p><label for='status'>Status</label><br/>";
-$options = array('active' => 'active', 'inactive' => 'inactive');
-echo form_dropdown('status',$options) ."</p>";
-
-echo form_submit('submit','create admin');
-echo form_close();
 
 
-?>
+<!DOCTYPE html>
+<html lang="en"></html>
+<h1><?php echo $judul; ?></h1>
+
+<form name='update_status' action='<?php echo base_url(); ?>index.php/dashboard/createAccount' method='post' >
+    <div id ="konten">
+        <div class="form-group">
+            <label for="idMurid">ID</label>
+            <input type="text" class="form-control" id="id_acc" placeholder="Masukkan ID">
+        </div>
+        <div class="form-group">
+            <label for="jam">Email</label>
+            <input type="text" class="form-control" id="email" placeholder="Masukkan Email">
+        </div>
+        <div class="form-group">
+            <label for="tanggal">Nama</label>
+            <input type="text" class="form-control" id="nama" placeholder="Masukkan Nama">
+        </div>
+        <div class="form-group">
+            <label for="tanggal">No. Telp</label>
+            <input type="text" class="form-control" id="no_telp" placeholder="Masukkan No Telp">
+        </div>
+          <div class="form-group">
+            <label for="tanggal">Role</label>
+            <input type="text" class="form-control" id="role" placeholder="Role">
+        </div>  
+       
+       
+
+   
+    <button type="submit" class="btn btn-danger">Create Account</button>
+</form>
+ </div>
+</div>
+
+</body>
+</html>
