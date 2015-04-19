2ab576a1901fbae3b0bef2f845ba8e23259fd492
idoooy <yodi.syaeful@gmail.com>
2015-04-19 22:53:23 +0700

    afaf

--- application/views/ops/feedback_home.php
+++ application/views/ops/feedback_home.php
@@ -1,16 +1,21 @@
+
 <h1><?php echo $judul; ?></h1>
-<p><?php echo anchor("admin/feedback/create", "Create new feedback"); ?></p>
+<ul class="nav nav-tabs">
+    <li role="presentation" ><a href="<?php echo base_url(); ?>index.php/dashboard/summary" > Home</a></li>
+    <li role="presentation" "><a href="<?php echo base_url(); ?>index.php/ops/student/searchSummary" > Student</a></li>
+    <li role="presentation"><a href="<?php echo base_url(); ?>index.php/dashboard/refunds" > Refund</a></li>
+    <li role="presentation"class="active"><a href="<?php echo base_url(); ?>index.php/dashboard/feedbacks" > Feedback</a></li>
+</ul><p><?php echo anchor("admin/refund/create", "Download"); ?></p>
 <?php
 if ($this->session->flashdata('message')) {
     echo "<div class='message'>" . $this->session->flashdata('message') . "</div>";
 }
 
-if (count($feedback)) {
-  echo "<table class='table table-bordered'>\n";
+if (count($feedback)) {  echo "<table id='table'>\n";
     echo "<tr valign='top'>\n";
-    echo "<th>ID Murid</th><th>ID Guru</th><th>ID Sales</th><th>Tanggal</th><th>Rating</th><th>Isi</th><th>Total Skor</th><th>Status</th>\n";
+    echo "<th>ID Murid</th><th>ID Guru</th><th>ID Sales</th><th>Tanggal</th><th>Rating</th><th>Isi</th><th>Total Skor</th><th>Status</th><th>Action</th>\n";
     echo "</tr>\n";
-    
+
     foreach ($feedback as $list) {
         echo "<tr valign='top'>\n";
         echo "<td>" . $list->Id_murid . "</td>\n";
@@ -22,8 +27,14 @@ if (count($feedback)) {
         echo "<td align='center'>" . $list->Total_skor . "</td>\n";
         echo "<td align='center'>" . $list->Status . "</td>\n";
         echo "</td>\n";
+    
+          echo "<td align='center'>";
+        echo anchor('admin/refund/edit/' . $list->id, 'edit');
+        echo " | ";
+        echo anchor('admin/refund/delete/' . $list->id, 'delete');
+        echo "</td>\n";
         echo "</tr>\n";
     }
     echo "</table>";
 }
-?>
+?>  
--- application/views/ops/refund_home.php
+++ application/views/ops/refund_home.php
@@ -1,12 +1,18 @@
 <h1><?php echo $judul; ?></h1>
-<p><?php echo anchor("ops/refund/create", "Create new refund"); ?></p>
+<ul class="nav nav-tabs">
+    <li role="presentation"><a href="<?php echo base_url(); ?>index.php/dashboard/summary" > Home</a></li>
+    <li role="presentation" "><a href="<?php echo base_url(); ?>index.php/ops/student/searchSummary" > Student</a></li>
+    <li role="presentation"class="active"><a href="<?php echo base_url(); ?>index.php/dashboard/refunds" > Refund</a></li>
+    <li role="presentation"><a href="<?php echo base_url(); ?>index.php/dashboard/feedbacks" > Feedback</a></li>
+</ul>
+<p><?php echo anchor("admin/refund/create", "Download"); ?></p>
 <?php
 if ($this->session->flashdata('message')) {
     echo "<div class='message'>" . $this->session->flashdata('message') . "</div>";
 }
 
 if (count($admins)) {
-    echo "<table class='table table-bordered'>\n";
+    echo "<table id='table'>\n";
     echo "<tr valign='top'>\n";
     echo "<th>Tanggal Refund</th><th>ID Murid</th><th>Jumlah Jam Hilang</th><th>Harga Per Jam</th><th>Sebab Jam Hilang</th><th>Action</th>\n";
     echo "</tr>\n";
