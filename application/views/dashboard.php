cc6a2d722041d95b549de157a3baa08d028e7b79
pamelanov <novranska111@gmail.com>
2015-04-21 15:17:12 +0700

    Revert "Revert "Merge remote-tracking branch 'origin/punya-derien' into punya-pamela""
    This reverts commit 71239eed9498a9e4c5dba71982ebdcded95d7f7a.

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -3,67 +3,10 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <title><?php echo $judul; ?></title>
-        <link href="<?php echo base_url(); ?>assets/bootstrap-3.3.4-dist/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
         <link href="<?php echo base_url(); ?>assets/css/style.css" rel="stylesheet" type="text/css" />
-        <!-- TinyMCE -->
-        <script type="text/javascript" src="<?php echo base_url(); ?>assets/TINYMCE/JSCRIPTS/TINY_MCE/tiny_mce_src.js"></script>
         <link href="<?php echo base_url(); ?>assets/bootstrap-3.3.4-dist/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-        <script type="text/javascript">
-            tinyMCE.init({
-                // General options
-                mode: "textareas",
-                theme: "advanced",
-                plugins: "safari,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template",
-                // Theme options
-                theme_advanced_buttons1: "save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,cut,copy,paste,pastetext,pasteword,|,search,replace,|",
-                theme_advanced_buttons2:
-                        "styleselect,formatselect,fontselect,fontsizeselect,forecolor,backcolor",
-                theme_advanced_buttons3: "bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|",
-                theme_advanced_toolbar_location: "top",
-                theme_advanced_toolbar_align: "left",
-                theme_advanced_statusbar_location: "bottom",
-                theme_advanced_resizing: true,
-                // Example content CSS (should be your site CSS)
-                content_css: "css/content.css",
-                // Drop lists for link/image/media/template dialogs
-                template_external_list_url: "lists/template_list.js",
-                external_link_list_url: "lists/link_list.js",
-                external_image_list_url: "lists/image_list.js",
-                media_external_list_url: "lists/media_list.js",
-                // Replace values for the template plugin
-                template_replace_values: {
-                    username: "Some User",
-                    staffid: "991234"
-                }
-            });
-
-            function convLinkVC(strUrl, node, on_save) {
-                strUrl = strUrl.replace("../", "");
-                return strUrl;
-            }
-
-            function ajaxLoad() {
-                var ed = tinyMCE.get('elm');
-
-                // Do you ajax call here, window.setTimeout fakes ajax call
-                ed.setProgressState(1); // Show progress
-                window.setTimeout(function () {
-                    ed.setProgressState(0); // Hide progress
-                    ed.setContent('HTML content that got passed from server.');
-                }, 3000);
-            }
-
-            function ajaxSave() {
-                var ed = tinyMCE.get('elm');
-
-                // Do you ajax call here, window.setTimeout fakes ajax call
-                ed.setProgressState(1); // Show progress
-                window.setTimeout(function () {
-                    ed.setProgressState(0); // Hide progress				
-                }, 3000);
-            }
-
-        </script>
+        
+        
         <!-- /TinyMCE -->
     </head>
     <body>
