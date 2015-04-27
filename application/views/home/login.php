edc30a23359b4bd2608457bc7d132a2fd3ff2500
pamelanov <novranska111@gmail.com>
2015-04-27 22:09:08 +0700

    ganti css sama nambah db

--- application/views/home/login.php
+++ application/views/home/login.php
@@ -25,7 +25,7 @@
 			<input type="password" class="form-control" name='password' placeholder="Password">
 			</div>
 	
-        <button type="submit" class="btn btn-warning">
+        <button type="submit" class="btn btn-info">
 			<span class="glyphicon glyphicon-hand-right" aria-hidden="true"></span>  Log in
 	</button></div>
 	
@@ -36,51 +36,15 @@
 </nav>
 
 <div id="loginbody">
-	Selamat datang di SICuT Ruangguru.
+	Sistem Informasi Customer Tracking Ruangguru
+	<div id ="coba"></div>
+	<div id="kontak-developer">
+		<p>Developed by Propensi B01</p><br/>
+		Pamela Novranska, <br/>
+		Derien Yauma, <br/>
+		Yodi Syaeful, <br/>
+		Pinkyvi Tiffany.
+	</div>
 </div>
    
-<!-- <nav class="navbar navbar-default">
-  <div class="container-fluid">
-
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <!-- <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav navbar-right">
-        <form name='login' "navbar-form navbar-right" role="login"
-	      action='<?php echo base_url();?>index.php/template/ceklogin' method='post' >
-			<div class="form-group">
-			<label for="exampleInputEmail1">ID</label>
-			<input type='text' class="form-control" name='id' placeholder="Enter ID">
-			</div>
-			<div class="form-group">
-			<label for="exampleInputPassword1">Password</label>
-			<input type="password" class="form-control" name='password' placeholder="Password">
-			</div>
-		<button type="submit" class="btn btn-warning">
-			<span class="glyphicon glyphicon-hand-right" aria-hidden="true"></span>  Submit
-		</button>
-	</form>
-       
-      </ul>
-    </div><!-- /.navbar-collapse -->
-  <!-- </div><!-- /.container-fluid 
-</nav>   -->
-<!--
-    
-<div id="text">
-	<h2>Please sign-in.</h2>
-	<form name='login' action='<?php echo base_url();?>index.php/template/ceklogin' method='post' >
-		<div class="form-group">
-			<label for="exampleInputEmail1">ID</label>
-			<input type='text' class="form-control" name='id' placeholder="Enter ID">
-		</div>
-		 <div class="form-group">
-			<label for="exampleInputPassword1">Password</label>
-			<input type="password" class="form-control" name='password' placeholder="Password">
-		</div>
-		<button type="submit" class="btn btn-warning">
-			<span class="glyphicon glyphicon-hand-right" aria-hidden="true"></span>  Submit
-		</button>
-	</form>
-</div>
--->
 </html>
