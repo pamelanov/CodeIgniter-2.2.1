0f72d10429295ecead461d81a58b08cdf1d67ffa
idoooy <yodi.syaeful@gmail.com>
2015-05-08 14:14:30 +0700

    lalala

--- application/views/ops/feedback_home.php
+++ application/views/ops/feedback_home.php
@@ -1,7 +1,7 @@
 
 <h1><?php echo $judul; ?></h1>
 <ul class="nav nav-tabs">
-    <li role="presentation" ><a href="<?php echo base_url(); ?>index.php/dashboard/summary" > Home</a></li>
+    
     <li role="presentation" "><a href="<?php echo base_url(); ?>index.php/ops/student/searchSummary" > Student</a></li>
     <li role="presentation"><a href="<?php echo base_url(); ?>index.php/dashboard/refunds" > Refund</a></li>
     <li role="presentation"class="active"><a href="<?php echo base_url(); ?>index.php/dashboard/feedbacks" > Feedback</a></li>
