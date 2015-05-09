9383ee1ec17a9f20a602e42711d9afe1b76abc99
pamelanov <novranska111@gmail.com>
2015-05-09 19:27:51 +0700

    ubah

--- application/views/supervisor/error_hasil_search.php
+++ application/views/supervisor/error_hasil_search.php
@@ -1,9 +1,10 @@
+  <h1><?php echo $judul; ?></h1>
  <ul class="nav nav-tabs">
   <li role="presentation"><a href="<?php echo base_url(); ?>index.php/supervisor/performance" > Create</a></li>
   <li role="presentation" class="active"><a href="<?php echo base_url(); ?>index.php/supervisor/performance/showEdit" > Edit </a></li>
   <li role="presentation"><a href="<?php echo base_url(); ?>index.php/supervisor/performance/overall" > Overall Performance</a></li>
  </ul>
 <div id="konten">
-    Tidak terdapat target dengan ID sales yang dimasukkan.<br/>
+    <p>Tidak terdapat target dengan ID sales yang dimasukkan.</p>
     <a href="<?php echo base_url(); ?>index.php/supervisor/performance/showEdit" > <button type="submit" class="btn btn-danger">Kembali</button> </a>
 </div>
