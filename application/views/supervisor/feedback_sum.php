3898f69613360d2e54185c4225ca2222a8f57a85
pamelanov <novranska111@gmail.com>
2015-05-22 02:11:48 +0700

    rapiin merge

--- application/views/supervisor/feedback_sum.php
+++ application/views/supervisor/feedback_sum.php
@@ -1,20 +1,14 @@
 <h1><?php echo $judul; ?></h1>
 
-
-<<<<<<< HEAD
 <div id="konten">
+     <h4>Periode: <?php echo $tanggal_awal . " s/d " . $tanggal_akhir; ?></h4>
     <?php
     if (count($feedbacks)) {
-        echo '<div class="panel panel-primary">
-=======
-    <div id="konten">
-        <h4>Periode: <?php echo $tanggal_awal . " s/d " . $tanggal_akhir; ?></h4>
-        <?php
+       
         echo '<div class="row">
                 <div class="col-md-8">';
-        if (count($feedbacks)) {
+        
             echo '<div class="panel panel-primary">
->>>>>>> punya-pamela
   <div class="panel-heading"><center>Rangkuman Feedback</center></div>';
         echo "<table class='table table-bordered'>\n";
         echo "<tr valign='top'>\n";
@@ -26,17 +20,6 @@
 
         echo "</tr>\n";
 
-<<<<<<< HEAD
-        foreach ($feedbacks as $list) {
-            echo "<tr valign='top'>\n";
-            echo "<td>" . $list->id_sales . "</td>\n";
-            echo "<td>" . $list->tanggal . "</td>\n";
-            echo "<td>" . $list->id_murid . "</td>\n";
-            echo "<td>" . $list->id_guru . "</td>\n";
-            echo "<td>" . $list->isi . "</td>\n";
-            echo "<td>" . $list->rating . "</td>\n";
-            echo "<td>" . $list->total_skor . "</td>\n";
-=======
             foreach ($feedbacks as $list) {
                 echo "<tr valign='top'>\n";
                 echo "<td>" . $list->id_sales . "</td>\n";
@@ -46,25 +29,18 @@
                 echo "<td>" . anchor('ops/feedbackCtrl/readFeedback/'.$list->id , 'Lihat') .
                         " | " . anchor('ops/feedbackCtrl/formUpdateFeedback/'.$list->id, 'Ubah') ."</td>\n";
 
->>>>>>> punya-pamela
-
 
             echo "</td>\n";
             echo "</tr>\n";
         }
-<<<<<<< HEAD
+
         echo "</table>	";
-        echo "</div>";
+        echo "</div></div>";
     }
 
     if (count($semua_ops)) {
-        echo '<div class="panel panel-primary">
-=======
-        echo "</div>";
         echo '<div class="col-md-4">';
-        if (count($semua_ops)) {
              echo '<div class="panel panel-primary">
->>>>>>> punya-pamela
                         <div class="panel-heading">
                             <center>Rangkuman Seluruh Operasional Sales</center></div>';
         echo "<table class='table table-bordered'>\n";
@@ -74,32 +50,20 @@
 
         echo "</tr>\n";
 
-<<<<<<< HEAD
-        foreach ($semua_ops as $list) {
-            echo "<tr valign='top'>\n";
-            echo "<td>" . $list->id_sales . "</td>\n";
-            echo "<td>" . $list->count_distinct('id_sales') . "</td>\n";
-=======
             foreach ($semua_ops as $list) {
                 echo "<tr valign='top'>\n";
                 echo "<td>" . $list->id_acc . "</td>\n";
                 echo "<td>" . $list->where('id_sales', $list->id_sales)->count() . "</td>\n";
->>>>>>> punya-pamela
 
 
             echo "</td>\n";
             echo "</tr>\n";
         }
-<<<<<<< HEAD
         echo "</table>	";
         echo "</div>";
     }
     ?>
-    <p><?php  echo anchor("download/download_Ofeedbacks/" . $tanggal_awal . "/" . $tanggal_akhir, "<button type='button' class='btn btn-primary'><span class='glyphicon glyphicon-download-alt' aria-hidden='true'></span> Download </button>"); ?></p>
-=======
-        echo '</div></div>';
-        ?>
         <p><?php // echo anchor("download/download_Ofeedbacks/" . $tanggal_awal . "/" . $tanggal_akhir, "<button type='button' class='btn btn-primary'><span class='glyphicon glyphicon-download-alt' aria-hidden='true'></span> Download </button>"); ?></p>
->>>>>>> punya-pamela
+
 
 </div>	
