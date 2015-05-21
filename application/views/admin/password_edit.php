93d76e3376dd61151adab2e1b62f98a3aa4069c6
idoooy <yodi.syaeful@gmail.com>
2015-05-22 00:17:28 +0700

    edit_password

--- application/views/admin/password_edit.php
+++ application/views/admin/password_edit.php
@@ -4,8 +4,12 @@
 <html lang="en"></html>
 <h1><?php echo $judul; ?></h1>
 
-<form name='update_status' action='<?php echo base_url(); ?>index.php/dashboard/editPassword' method='post' onsubmit='return confirm("Apakah Anda yakin ingin mengubah password?")'>
+<form name='update_password' action='<?php echo base_url(); ?>index.php/dashboard/editPassword' method='post' onsubmit='return confirm("Apakah Anda yakin ingin mengubah password?")'>
     <div id ="konten">
+         <div class="form-group">
+            <input type="hidden" class="form-control" name="id_acc" placeholder="Masukkan ID" value="<?php echo $user->id_acc?>" >
+        </div>
+        
         <div class="form-group">
             <label for="password">Password</label>
             <input type="password" class="form-control" name="password" placeholder="Masukkan Password Baru" >
